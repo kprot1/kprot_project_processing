c600954bcd849b1f733c0838e869019adc682a51
kprot <kirikpv12@yandex.ru>
2018-09-08 14:10:28 +0300

    Настройка сервисов в services.xml, перенос в отдельный конфиг. Настроил MyController и BalanceCalculationService

---
+++ src/App/Services/BalanceCalculationService.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tutyo
+ * Date: 08.09.2018
+ * Time: 13:58
+ */
+
+namespace App\Services;
+
+
+class BalanceCalculationService
+{
+    public function calculate()
+    {
+
+    }
+}
